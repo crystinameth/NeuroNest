8ef99662919ca712e1357d57fdf74bf24cd6cfc7
shivimishra <shivimishra130103@gmail.com>
2024-02-16 18:01:08 +0530

    Auth: using clerk

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function RootPage() {
+    return (
+      <div>
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    );
+  }
+  
