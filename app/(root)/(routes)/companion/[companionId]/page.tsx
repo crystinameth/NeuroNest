535770b5a398eae152a6e736b7b65f48d979d396
shivimishra <shivimishra130103@gmail.com>
2024-02-18 20:10:16 +0530

    client issue fixed

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,21 +1,29 @@
 import prismadb from "@/lib/prismadb";
 import { CompanionForm } from "./components/companion-form";
+import { Companion } from "@prisma/client";
 
 interface CompanionIdPageProps {
   params: {
-    companionId: string;
+    companionId: string | "new";
   };
 }   
 
 const CompanionIdPage = async ({ params }: CompanionIdPageProps) => {
   //TODO: Check subscription
 
-  const companion = await prismadb.companion.findUnique({
-    where: {
-      id: params.companionId,
-    },
-  });
+  let companion: Companion | null;
 
+  if(params.companionId === "new") {
+    companion = null;
+  }
+  else{
+    companion = await prismadb.companion.findUnique({
+      where: {
+        id: params.companionId,
+      },
+    });
+  }
+  
   const categories = await prismadb.category.findMany();
 
   return <CompanionForm initialData={companion} categories={categories} />;
