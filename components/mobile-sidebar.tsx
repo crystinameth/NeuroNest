8a30cff3067bf2afd511e1ddc44076965eaa7406
shivimishra <shivimishra130103@gmail.com>
2024-02-23 22:54:26 +0530

    sidebar fix

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -18,7 +18,7 @@ export const MobileSidebar = ({
                 <Menu />
             </SheetTrigger>
             <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-                <Sidebar isPro={isPro}/>
+                <Sidebar isPro={isPro} />
             </SheetContent>
         </Sheet>
     )
