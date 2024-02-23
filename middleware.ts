c4e9e84325d88f1017ee5caa92b85d6801de5626
shivimishra <shivimishra130103@gmail.com>
2024-02-23 20:35:58 +0530

    stripe backend completed

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
  
-export default authMiddleware({});
+export default authMiddleware({
+  publicRoutes: ["/api/webhook"],           //becuz stripe needs access to this route , as we pass the userId as metadata and we don't need auth for this route
+});
  
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
